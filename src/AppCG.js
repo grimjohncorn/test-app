09d618be6afddb63e2f78b4408e9f3f157c881ba
grimjohncorn <chris.grimley@hotmail.co.uk>
2019-04-11 07:57:45 +0100

    working on isLoading

--- src/AppCG.js
+++ src/AppCG.js
@@ -5,21 +5,23 @@ class RetrieveData extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            dataReturned: {}
+            dataReturned: {},
+            isLoading: true
         }
     }
     
     componentDidMount() {
+        this.setState({isLoading: true})
         fetch('https://swapi.co/api/people/1')
             .then(response => response.json())
             .then(data => this.setState({dataReturned: data}))
-
+            .then(this.setState({isLoading: false}))
     }
 
     render() {
         return (
             <div>
-                {this.state.dataReturned.name}
+                {this.state.isLoading ? <p>Loading...</p> : this.state.dataReturned.name}
             </div>
         )
     }
@@ -36,7 +38,7 @@ class AppCG extends React.Component {
         return (
             <div id="testTitle">
                 <h1>testApp</h1>
-                <p><RetrieveData /></p>
+                    <p><RetrieveData /></p>
                 <p>test2</p>
             </div>
 
