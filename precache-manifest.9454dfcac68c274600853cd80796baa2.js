8d7c42c3ebfaad26a7b9c26baa376dc59371c2cb
grimjohncorn <chris.grimley@hotmail.co.uk>
2019-04-14 18:51:37 +0100

    Updates

---
+++ precache-manifest.9454dfcac68c274600853cd80796baa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa6e43e27dacf12ffab3",
+    "url": "/test-app/static/css/main.11698b8a.chunk.css"
+  },
+  {
+    "revision": "aa6e43e27dacf12ffab3",
+    "url": "/test-app/static/js/main.06cc26bf.chunk.js"
+  },
+  {
+    "revision": "7318f3809038590af7ec",
+    "url": "/test-app/static/js/runtime~main.e9adfd5a.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/test-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "9a7bf4b45a784e64d518822a42d74a9b",
+    "url": "/test-app/index.html"
+  }
+];
